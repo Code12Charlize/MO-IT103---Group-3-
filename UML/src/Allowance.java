0e7e9e898d9cad057d36c1e962d4d4f021fec82d
charlizehub <lr.cbactong@mmdc.mcl.edu.ph>
2025-05-19 20:16:25 +0800

    Updated UML

---
+++ UML/src/Allowance.java
@@ -0,0 +1,13 @@
+public class Allowance {
+    float riceSubsidy;
+    float phoneAllowance;
+    float clothingAllowance;
+
+    public float calculateSalaryDeduction() {
+
+    }
+
+    public float updateSalaryDeductions() {
+
+    }
+}
